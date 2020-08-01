a33373be97189fd63640724ea04e518bf1e78c9b
chaitanyapotti <chaitanya.potti@gmail.com>
2020-08-01 19:33:16 +0800

    Main direct sdk class complete

--- torusdirect/src/test/java/org/torusresearch/torusdirecttest/handlers/GoogleHandlerTest.java
+++ torusdirect/src/test/java/org/torusresearch/torusdirecttest/handlers/GoogleHandlerTest.java
@@ -1,15 +1,10 @@
 package org.torusresearch.torusdirecttest.handlers;
 
-import org.junit.jupiter.api.Test;
-import org.torusresearch.torusdirect.handlers.GoogleHandler;
-
-import java.util.UUID;
-
 public class GoogleHandlerTest {
 
-    @Test
-    public void getsCorrectFinalUrl() {
-        GoogleHandler handler = new GoogleHandler(UUID.randomUUID().toString(), "abcd-123", "https://app.tor.us/redirect", "google");
-        System.out.println(handler.getFinalUrl());
-    }
+//    @Test
+//    public void getsCorrectFinalUrl() {
+//        GoogleHandler handler = new GoogleHandler(UUID.randomUUID().toString(), "abcd-123", "https://app.tor.us/redirect", "google");
+//        System.out.println(handler.getFinalUrl());
+//    }
 }
