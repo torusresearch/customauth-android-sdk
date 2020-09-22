b245102a2b91723597fd65446a3aeba4532ab01c
Sandeep Gupta <sandeep.tokenizer@gmail.com>
2020-09-22 22:55:39 +0530

    fix cancel issue

--- torusdirect/src/main/java/org/torusresearch/torusdirect/activity/StartUpActivity.java
+++ torusdirect/src/main/java/org/torusresearch/torusdirect/activity/StartUpActivity.java
@@ -12,17 +12,19 @@ import org.torusresearch.torusdirect.R;
 import org.torusresearch.torusdirect.interfaces.ILoginHandler;
 
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class StartUpActivity extends AppCompatActivity {
     public static final String URL = "URL";
     public static AtomicReference<ILoginHandler> loginHandler = new AtomicReference<>();
+    private AtomicBoolean isLoginStep = new AtomicBoolean();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
-
+        isLoginStep.set(true);
         CustomTabsIntent intent = new CustomTabsIntent.Builder().build();
         String data = getIntent().getStringExtra(URL);
         if (data == null) {
@@ -30,7 +32,16 @@ public class StartUpActivity extends AppCompatActivity {
             data = getIntent().getDataString();
         }
         intent.launchUrl(this, Uri.parse(data));
-        startActivity(intent.intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!isLoginStep.get()) {
+            finish();
+        } else {
+            isLoginStep.set(false);
+        }
     }
 
     @Override
