5044dd5ea436b6eec627173a2fe6a48f06d782a5
Gaurav Goel <grvgoel19@gmail.com>
2023-11-01 09:34:58 +0530

    refactor: removing redundant code and update fnd and torus utils java version.
    Signed-off-by: Gaurav Goel <gaurav@tor.us>

--- sdk/src/main/java/org/torusresearch/customauth/types/CustomAuthArgs.java
+++ sdk/src/main/java/org/torusresearch/customauth/types/CustomAuthArgs.java
@@ -1,18 +1,10 @@
 package org.torusresearch.customauth.types;
 
-import org.torusresearch.fetchnodedetails.FetchNodeDetails;
 import org.torusresearch.fetchnodedetails.types.TorusNetwork;
 
 import java.util.HashMap;
 
 public class CustomAuthArgs {
-
-    public static HashMap<TorusNetwork, String> CONTRACT_MAP = new HashMap<TorusNetwork, String>() {{
-        put(TorusNetwork.MAINNET, FetchNodeDetails.PROXY_ADDRESS_MAINNET);
-        put(TorusNetwork.TESTNET, FetchNodeDetails.PROXY_ADDRESS_TESTNET);
-        put(TorusNetwork.CYAN, FetchNodeDetails.PROXY_ADDRESS_CYAN);
-        put(TorusNetwork.AQUA, FetchNodeDetails.PROXY_ADDRESS_AQUA);
-    }};
 
     public static HashMap<TorusNetwork, String> SIGNER_MAP = new HashMap<TorusNetwork, String>() {{
         put(TorusNetwork.MAINNET, "https://signer.tor.us");
